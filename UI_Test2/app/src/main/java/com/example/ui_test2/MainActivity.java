723525d02998a2c192297a5c76e9e40deadf6faf
terry0803 <terryruei0803@gmail.com>
2024-03-22 14:59:03 +0800

    0322

--- UI_Test2/app/src/main/java/com/example/ui_test2/MainActivity.java
+++ UI_Test2/app/src/main/java/com/example/ui_test2/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.ui_test;
+package com.example.ui_test2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import com.example.ui_test2.DisplayStringActivity;
 
 public class MainActivity extends AppCompatActivity {
 
